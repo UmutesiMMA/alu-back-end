409992994ece0ce07319850b95aaf639e85c3e98
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-07-18 23:04:43 +0200

    change

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Get TODOs"""
-
-import json
+""" Library to gather data from an API """
 
 import requests
 import sys
 
-if __name__ == '__main__':
+""" Script to return a given employee ID
+together with their TODO list progress
+"""
+
+if __name__ == "__main__":
     employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
     todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
     todo = todo.format(employee_id)
 
-    user_data = requests.request("GET", url).json()
-    todos = requests.request("GET", todo).json()
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
 
-    employee_name = user_data.get("name")
-    total_tasks = list(filter(lambda x: (x["completed"] is True), todos))
+    employee_name = user_info.get("name")
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
     task_com = len(total_tasks)
-    total_task_done = len(todos)
+    total_task_done = len(todo_info)
 
     print(
         "Employee {} is done with tasks({}/{}):".format(

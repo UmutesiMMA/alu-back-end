4482c5b1f41e7b48e5beff142e87bac4647aa26a
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-07-18 22:57:57 +0200

    minor change

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,13 +9,14 @@ import sys
 if __name__ == '__main__':
     user_url = ("https://jsonplaceholder.typicode.com/users/{}".
                 format(sys.argv[1]))
-    user_data = requests.get(user_url)
+    user_data = requests.get(user_url).json()
+    user_name = user_data.get('name')
     todos_url = ("https://jsonplaceholder.typicode.com/todos?userId={}".
                  format(sys.argv[1]))
-    todos = json.loads(requests.get(todos_url).text)
+    todos = requests.get(todos_url).json()
     completed_todos = list(filter(lambda todo: todo['completed'], todos))
     print('Employee {} is done with tasks({}/{}):'.format(
-        json.loads(user_data.text).get('name'),
+        user_name,
         len(completed_todos),
         len(todos)))
     for item in completed_todos:
